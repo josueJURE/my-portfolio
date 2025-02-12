ff8093a2f1aa27524af541884fb3e6ff1ecca5a1
Josue JURE <57095041+josueJURE@users.noreply.github.com>
2025-02-12 11:49:52 +0000

    remove unecessary code

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+
+
 const menuIcon = document.querySelector(".menu-icon");
 const container = document.querySelector(".container");
 
 menuIcon.addEventListener("click", () => {
     container.classList.toggle("change")
+    console.log("clicked")
 })
+
+
+
+
+
+
