8e0a89cc11d725f6c6ab0602a96fbd6ab91740e2
josueJURE <josue.jure@gmail.com>
2025-03-14 11:33:02 +0000

    feat: update copyright year dynamically in footer

--- public/index.js
+++ public/index.js
@@ -2,7 +2,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menuIcon = document.querySelector(".menu-icon");
   const container = document.querySelector(".container");
+  const year = document.querySelector(".year");
+  const currentYear = new Date().getFullYear();
 
+  year.innerHTML = `&copy; Josué<span>JURE</span> All Rights Reserved. ${currentYear} `
 
   menuIcon.addEventListener("click", () => {
     container.classList.toggle("change");
