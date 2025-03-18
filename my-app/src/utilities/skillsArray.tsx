9045f3bdf130a54b748bdadf32fe48a30036ef8e
josueJURE <josue.jure@gmail.com>
2025-03-18 12:05:33 +0000

    refactor: simplify skill descriptions in skillsData

--- my-app/src/utilities/skillsArray.tsx
+++ my-app/src/utilities/skillsArray.tsx
@@ -12,37 +12,37 @@ const skillsData: SkillsDataType = [
     {
       skill: "HTML",
       percentage: "95%",
-      description: "Mastery in HTML is the cornerstone of my web development journey. With over a year of self-teaching, I've honed my skills in creating clean, semantic, and well-structured HTML."
+      description: "Mastery in HTML is the cornerstone of my web development journey. With over a year of self-teaching, I've honed my skills in creating clean, well-structured HTML."
     },
     {
       skill: "CSS",
       percentage: "90%",
-      description: "My CSS skills extends beyond styling; it's about creating visually appealing and responsive designs. Through self-guided learning, I've become adept at leveraging CSS."
+      description: "My CSS skills extends beyond styling; it's about creating visually appealing and responsive designs."
     },
     {
       skill: "JavaScript",
       percentage: "85%",
-      description: "JavaScript is where I thrive in adding interactivity and dynamism to web applications. Over the past year, I've immersed myself in the language to master ES6 concepts."
+      description: "JavaScript is where I thrive in adding interactivity and dynamism to web applications. I've immersed myself in the language to master ES6 concepts."
     },
     {
       skill: "TypeScript",
       percentage: "75%",
-      description: "After mastering JavaScript, learning TypeScript felt like the next natural step. I now understand how using types can prevent bugs from propagating into the codebase"
+      description: "After mastering JavaScript, learning TypeScript felt like the next natural step. I now understand how using types can prevent bugs from propagating."
     },
     {
       skill: "Node.js and Express",
       percentage: "85%",
-      description: "The dynamic duo of Node.js and Express has revolutionized my web development workflow. With Node.js as the backbone, I can leverage JavaScript's versatility on the server-side."
+      description: "The dynamic duo of Node.js and Express has revolutionized my web development workflow. I can now leverage JavaScript's versatility on the server-side."
     },
     {
       skill: "ChatGPT OpenAI API",
       percentage: "75%",
-      description: "The ChatGPT OpenAI API is a recent technology that's captured my interest. I've integrated it into at least two of my projects, and the results have been nothing short of remarkable."
+      description: "The ChatGPT OpenAI API is a recent technology that's captured my interest. I've integrated it into two of my projects, and the results have been remarkable."
     },
     {
       skill: "React",
       percentage: "50%",
-      description: "Since starting to volunteer with CAFOD, I've learned a lot more about React and React hooks such as useContext. I've also been exposed to React libraries and frameworks such as Next.js"
+      description: "Since starting to volunteer with CAFOD, I've learned a lot more about React and React hooks such as useContext. I've also been exposed to a Next.js"
     }
   ];
 
