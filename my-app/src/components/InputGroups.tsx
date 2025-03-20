51aea52558cdc03c519a0c0df103c15f39751cbf
josueJURE <josue.jure@gmail.com>
2025-03-20 10:40:27 +0000

    fix: add key prop to map iteration in InputGroups component for improved rendering

--- my-app/src/components/InputGroups.tsx
+++ my-app/src/components/InputGroups.tsx
@@ -5,7 +5,7 @@ function InputGroups() {
     <>
       <div className="input-groups">
         {inputGroupData.map((item, index) => (
-          <div className={item.className}>
+          <div className={item.className} key={index}>
             <label htmlFor={item.label}>
               {item.innerHTML} <i className="fa-solid fa-asterisk"></i>
             </label>
