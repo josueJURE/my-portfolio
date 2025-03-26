4195e63ce1c43d3b8e8d2c12e6be6fe1c5e58495
josueJURE <josue.jure@gmail.com>
2025-03-26 12:36:35 +0000

    Save changes before switching branches

---
+++ my-app/src/components/SkillsCard.tsx
@@ -0,0 +1,21 @@
+import skillsData from "../utilities/skillsArray";
+import { JSX } from "react";
+
+function SkillsCard(): JSX.Element {
+  return (
+    <div className="skills-cards">
+      {skillsData.map((item, index) => (
+        <div className="skills-card" key={index}>
+          <div className="skill">
+            <span>{item.skill}</span>
+            <span>{item.percentage}</span>
+          </div>
+          <p>{item.description}</p>
+          <div className="skills-card-progress"></div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default SkillsCard;
