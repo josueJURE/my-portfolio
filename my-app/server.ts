00de4c49bc572ffb7ad507ed017ef5ce29641f1d
josueJURE <josue.jure@gmail.com>
2025-04-12 11:30:30 +0100

    feat:server migrate server.js to TypeScript

--- my-app/server.ts
+++ my-app/server.ts
@@ -6,25 +6,31 @@ const path = require("path");
 const nodemailer = require("nodemailer");
 const cors = require("cors"); //Add this line
 app.use(cors()); //Add this line
-const { emailTemplate, confirmationMessage, sendErrorMessage} = require("./utilities/functions.js");
+const {
+  emailTemplate,
+  confirmationMessage,
+  sendErrorMessage,
+} = require("./utilities/functions.js");
+
+// Ensure confirmationMessage is a function
+
 const htmlStyle = require("./utilities/htmlStyle.js");
 
 app.use(express.urlencoded({ extended: true }));
 
-const {PrismaClient} = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+import { Request, Response } from "express";
 
-
-
-
-app.post("/", (req, res) => {
-  console.log(
-    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
-  );
+app.post("/", (req: Request, res: Response) => {
+  console
+    .log
+    // `${req.method} ${req.secure ? "https" : "http"}://${req.get("host")}${req.originalUrl}`
+    ();
 
   try {
-    const {name, email, subject, message } = req.body;
+    const { name, email, subject, message } = req.body;
 
     console.log("backend server", req.body);
     const transporter = nodemailer.createTransport({
@@ -34,8 +40,6 @@ app.post("/", (req, res) => {
         pass: process.env.third_party_app_password,
       },
     });
-
- 
 
     const emailDocument = emailTemplate(message, email, name);
 
@@ -57,7 +61,12 @@ app.post("/", (req, res) => {
 
     main();
 
-    async function createUser(param1, param2, param3, param4) {
+    async function createUser(
+      param1: string,
+      param2: string,
+      param3: string,
+      param4: string
+    ) {
       const user = await prisma.user.create({
         data: {
           fullName: param1,
@@ -69,35 +78,25 @@ app.post("/", (req, res) => {
       console.log("User created:", user);
     }
 
-   
-    createUser( name, email, subject, message)
-      .then(async() => {
+    createUser(name, email, subject, message)
+      .then(async () => {
         await prisma.$disconnect();
         console.log("User created successfully");
       })
-      .catch(async(error) => {
+      .catch(async (error) => {
         console.error("Error creating user:", error);
         await prisma.$disconnect();
         process.exit(1);
       });
-
-
-
-
   } catch (error) {
-    res.status(500).send(sendErrorMessage(htmlStyle));
+    res.send(sendErrorMessage(htmlStyle));
   }
-
 });
-
-
-
-
 
 // This should be after all your other routes
 // Serve your React app's index.html for all other GET requests
 app.use(express.static(path.join(__dirname, "dist")));
-app.get("*", (req, res) => {
+app.get("*", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
@@ -106,5 +105,3 @@ const port = process.env.PORT || 4001;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-
