afd03267bae4bdcd1f9b18bffdc247d270da8d5a
josueJURE <josue.jure@gmail.com>
2025-05-20 11:48:35 +0100

    (feat:js) use audio HTML element to play mp3 file

--- src/components/TextToSpeechControls .tsx
+++ src/components/TextToSpeechControls .tsx
@@ -19,18 +19,18 @@ function TextToSpeechControls() {
 
   return (
     <div className="recording">
-      <FaPlay size={30} />
+      {/* <FaPlay size={30} />
       <FaCirclePause size={30} />
       <FaRegStopCircle size={30} />
-      <IoSpeedometer size={30} />
-
-      <source className="audio_source" type="audio/mpeg"></source>
+      <IoSpeedometer size={30} /> */}
+      <audio style={{ width: '25rem', height: '9rem',}} controls src="./speech.mp3"></audio>
+      <source className="audio_source" type="audio/mpeg" src="./speech.mp3"/>
       <i className="fa-solid fa-microphone" data-name="microphone"></i>
       <i className="fa-solid fa-pause" data-name="pause"></i>
       <i className="fa-solid fa-stop" data-name="stop"></i>
       <div className="speed-wrapper">
-        <label htmlFor="speed">Speed</label>
-        <input onChange={handleCount}
+        {/* <label htmlFor="speed">Speed</label> */}
+        {/* <input onChange={handleCount}
           type="number"
           name="speed"
           id="speed"
@@ -38,7 +38,7 @@ function TextToSpeechControls() {
           max="2"
           step="0.25"
           value={count}
-        ></input>
+        ></input> */}
       </div>
     </div>
   );
